3c51abb101ee8665bd7b12cafdba397d3d17b401
JerryXie96 <hongcheng.xie@my.cityu.edu.hk>
2021-08-31 16:48:34 +0800

    Addition (the code about socket in server and client)

---
+++ params.h
@@ -0,0 +1,3 @@
+#define INT_LENGTH 32   // the length of one integer (i.e., data)
+#define BLOCK_SIZE 2    // the length of one block in tokens and indexes
+#define KEY_NUM 2       // the number of keys for one item
