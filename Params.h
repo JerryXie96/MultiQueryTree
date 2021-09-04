1d889b1a2580c4d9a27f28e95728c5fcf4a348df
JerryXie96 <hongcheng.xie@my.cityu.edu.hk>
2021-09-04 19:32:30 +0800

    addition: the structure of TreeNode, Plaintext Element, and new parameter of the number of ciphers in one block

--- Params.h
+++ Params.h
@@ -1,7 +1,8 @@
 #ifndef PARAM_H
     #define PARAM_H
-    #define INT_LENGTH 32   // the length of one integer (i.e., data)
-    #define BLOCK_SIZE 2    // the length of one block in tokens and indexes
-    #define KEY_NUM 2       // the number of keys for one item
-    #define HASH_LENGTH 32  // the length of hash value
+    #define INT_LENGTH 32                               // the length of one integer (i.e., data)
+    #define BLOCK_SIZE 2                                // the length of one block in tokens and indexes
+    #define KEY_NUM 2                                   // the number of keys for one item
+    #define HASH_LENGTH 32                              // the length of hash value
+    #define BLOCK_CIPHER_NUM ((1<<BLOCK_SIZE)-1)        // the number of ciphers in one index block
 #endif  /* PARAM_H */
--- TreeNode.h
+++ TreeNode.h
@@ -1,9 +1,26 @@
+#include "Params.h"
+
+// the structure of one block in the index
+typedef struct {
+    unsigned char blockCipher[BLOCK_CIPHER_NUM][HASH_LENGTH]; 
+} Block;
+
+// the structure of one key in the index
+typedef struct {
+    Block block[BLOCK_SIZE];
+} IndexKey;
+
 typedef struct TN {
-    int id;             // the id of this node
-    short selKey;       // the index of selected key 
+    int id;                         // the id of this node
+    short selKey;                   // the index of selected key 
+    IndexKey indexKey[KEY_NUM];     // the indexes for all keys for one tree node
 
-    
-
-    TN* leftPointer;    // the pointer of its left son
-    TN* rightPointer;   // the pointer of its right son
+    TN* leftPointer;                // the pointer of its left son
+    TN* rightPointer;               // the pointer of its right son
 } TreeNode;
+
+// the structure of plaintext data element
+typedef struct {
+    int id;                         // the id of this element
+    unsigned int data[KEY_NUM];     // the data for all the keys in this element
+} PlainElement;
