c437e6043e7f086670410d4c02c1049cab9b88e3
JerryXie96 <hongcheng.xie@my.cityu.edu.hk>
2021-09-05 20:22:06 +0800

    addition: the quick sort functions for plaintext element list

--- TreeNode.h
+++ TreeNode.h
@@ -24,3 +24,38 @@ typedef struct {
     int id;                         // the id of this element
     unsigned int data[KEY_NUM];     // the data for all the keys in this element
 } PlainElement;
+
+// the partition function for quick sort
+int partition(PlainElement* plainElementList, short selKeyThisLayer, int low, int high){
+    PlainElement pivot=plainElementList[high];
+    int i=low-1;
+
+    for(int j=low;j<=high-1;j++){
+        if(plainElementList[j].data[i]<pivot.data[i]){
+            i++;
+            PlainElement t=plainElementList[i];
+            plainElementList[i]=plainElementList[j];
+            plainElementList[j]=t;
+        }
+    }
+    PlainElement t=plainElementList[i+1];
+    plainElementList[i+1]=plainElementList[high];
+    plainElementList[high]=t;
+    return i+1;
+}
+
+// quick sort function to sort the plaintext element list according to the key selKeyThisLayer
+void quickSort(PlainElement* plainElementList, short selKeyThisLayer, int low, int high){
+    if(low<high){
+        int p=partition(plainElementList,selKeyThisLayer,low,high);
+
+        quickSort(plainElementList,selKeyThisLayer,low,p-1);
+        quickSort(plainElementList,selKeyThisLayer,p+1,high);
+    }
+    return;
+}
+
+// build the tree structure based on the plaintext element list
+TreeNode* buildTree(PlainElement* plainElementList, short selKeyThisLayer){
+
+}
